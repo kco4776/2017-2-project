f0305884ae8e0ed8b891ad1b749e93e9092a13c9
kco4776 <kco4776@naver.com>
2017-12-08 18:56:05 +0900

    임시 commit
    commit

--- project/test.c
+++ project/test.c
@@ -2,28 +2,16 @@
 #include <stdlib.h>
 #include <string.h>
 
-typedef struct node {
-  char *name;
-  struct node *next;
-} node;
-
 int main(void) {
-  node *p, *tmp;
-  p = (node *)malloc(sizeof(char));
-  p->name = (char *)calloc(50, sizeof(char));
-  p->next = (node *)malloc(sizeof(char));
-  p->next->name = (char *)calloc(50, sizeof(char));
-  p->next->next = NULL;
-  p->name = "string";
-  p->next->name = "string2";
-  tmp = p;
-  printf("1:%s\n", tmp->name);
-  tmp = tmp->next;
-  printf("2:%s\n", tmp->name);
-  tmp->next = (node *)malloc(sizeof(node));
-  tmp->next->name = (char *)calloc(50, sizeof(char));
-  tmp->next->next = NULL;
-  tmp->next->name = "string3";
-  printf("3:%s", p->next->next->name);
+  char *names;
+  names = (char *)calloc(100, sizeof(char));
+  char *name;
+  name = (char *)calloc(50, sizeof(char));
+  printf("입력:");
+  fgets(names, 100, stdin);
+  printf("names:%s\n", names);
+  name = strtok(names, ",");
+  printf("%s\n", name);
 
+  return 0;
 }
